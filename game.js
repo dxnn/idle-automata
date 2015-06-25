dd1c4bc323151192ee6c59bd119832727a5f298b
dann toliver <dann@bentobox.net>
2015-06-25 00:05:29 -0400

    explosions in the pre

--- game.js
+++ game.js
@@ -3,40 +3,76 @@
 var w = 40
 var h = 40
 var grid = []
-var ducats = 0
+var last = 0
+var rate = 1000
+var ducats = 1
 var paused = false
 var handlers = {}
+var el_grid, el_ducats
 
 function tick() {
   // poke each cell, in arbitrary order
+  for(var i=0; i < w*h; i++) {
+    var cell = grid[i]
+    if(cell.handler) break
+    run(cell, 'go')
+    if(!cell.life) {
+      run(cell, 'post')
+      spawn(cell, " ")
+    }
+  }
 }
 
 function tock(n) {
   // do n ticks
+  for(var i=0; i < n; i++) {
+    tick()
+  }
 
   // update last tock
+  last = Date.now()
 }
 
 function render() {
   // update cell display
+  render_cells()
 
-  // update counter display
+  // update stats display
+  render_stats()
+}
+
+function render_cells() {
+  el_grid.textContent = grid_to_string()
+}
+
+function grid_to_string() {
+  var str = ""
+  for(var i=0; i < w*h; i++) {
+    if(!i%w) str += "\n"
+    str += grid[i].char
+  }
+  return str
+}
+
+function render_stats() {
+  el_ducats.textContent = ducats
 }
 
 function time_to_tock(ms) {
   // when was our last tock?
+  var diff = ms - last
 
   // how many ticks should we do?
+  var ticks = diff / rate
 
   // call tock
+  tock(ticks)
 }
 
 function loop() {
-  // if not paused
-
-  // get current time
-
-  // call time_to_tock(ms)
+  if(paused) return
+  var time = Date.now()         // get current time
+  time_to_tock(time)
 }
 
 function looper() {
@@ -52,12 +88,49 @@ function set_type(cell, type) {
 function init() {
   // set up grid
   for(var i=0; i < w*h; i++) {
-    grid[i] = new_cell()
+    grid[i] = new_cell(i)
   }
 
   // set up stats
+  last = Date.now()
+  el_grid = document.getElementById('grid')
+  el_ducats = document.getElementById('ducats')
 
   // set up nabes and diags
+  for(var i=0; i < w*h; i++) {
+    add_nabes(grid[i], i)
+    add_diags(grid[i], i)
+  }
+
+  build_archetypes()
+}
+
+function add_nabes(cell, index) {
+  cell.nabes.push(get_torus_cell(index, 0, -1))
+  cell.nabes.push(get_torus_cell(index, 0,  1))
+  cell.nabes.push(get_torus_cell(index, -1, 0))
+  cell.nabes.push(get_torus_cell(index,  1, 0))
+}
+
+function add_diags(cell, index) {
+  cell.nabes.push(get_torus_cell(index, 1,  -1))
+  cell.nabes.push(get_torus_cell(index, 1,   1))
+  cell.nabes.push(get_torus_cell(index, -1,  1))
+  cell.nabes.push(get_torus_cell(index, -1, -1))
+}
+
+function get_torus_cell(index, dx, dy) {
+  if(dx == -1 && index%w == 0)
+    dx = w-1
+  if(dx ==  1 && index%w == w-1)
+    dx = 1-w
+
+  if(dy == -1 && index < w)
+    dy = h-1
+  if(dy ==  1 && index > w*(h-1))
+    dy = 1-h
+
+  return grid[index + dx + dy*w]
 }
 
 function build_archetypes() {
@@ -87,23 +160,43 @@ function build_archetypes() {
   charloop('b', 'z', function(char) {
     addhand(char, 'go', function(cell) {
       var free = find_free_nabe(cell)
-      if(!free) return false
+      if(!free) return
       cell.life -= 1
       spawn(free, String.fromCharCode(char.charCodeAt() - 1))
     })
   })
 }
 
+function find_free_nabe(cell) {
+  // TODO: check in arbitrary order
+  var nabe
+  if(nabe = get_torus_cell(cell.index, 0,  1)) return nabe
+  if(nabe = get_torus_cell(cell.index, 0, -1)) return nabe
+  if(nabe = get_torus_cell(cell.index, 1,  0)) return nabe
+  if(nabe = get_torus_cell(cell.index, -1, 0)) return nabe
+  return false
+}
+
+function run(cell, handler) {
+  var hands = cell.handlers[handler]
+  var len = hands.length
+  for(var i=0; i<len; i++)
+    hands[i](cell)
+}
+
 function spawn(cell, char) {
   cell.char = char
   cell.handler = handlers[char]
-  cell.handler.init(cell)
+  run(cell, 'init')
 }
 
-function new_cell() {
+function new_cell(i) {
   return { char: " "
          , handler: false
          , life: 0
+         , index: i
+         , nabes: []
+         , diags: []
          // , skip: 0
          }
 }
@@ -143,3 +236,5 @@ function clone(obj) {
 function log(x) {
   console.log(x)
 }
+
+init()
